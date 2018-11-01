83084f8770c945c988691ff6e920bb255c853312
SystemGlitch <jeremy.la@outlook.fr>
2018-11-01 12:41:13 +0100

    Check if function declaration is prototype or not

--- include/parsing_functions.h
+++ include/parsing_functions.h
@@ -4,7 +4,7 @@
 #include "stringutils.h"
 #include "scopetree.h"
 
-function_t *function_init(char *name, char *type, unsigned char type_is_pointer, arraylist_t *params);
+function_t *function_init(char *name, unsigned char is_prototype, char *type, unsigned char type_is_pointer, arraylist_t *params);
 function_t *get_function_from_declaration(char *line);
 
 void function_free(function_t *function);
