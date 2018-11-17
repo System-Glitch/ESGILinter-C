8d41319b5e36371c965c47ab0564d0edad8a52ad
Moussier <arthur.bensimon@gmail.com>
2018-11-17 19:31:36 +0100

    add max_line_numbers rule.

--- src/rules/max_line_numbers.c
+++ src/rules/max_line_numbers.c
@@ -8,6 +8,7 @@
 //returns number of warnings
 unsigned int max_line_numbers(arraylist_t *file, unsigned int n){
   unsigned int counter = 0;
+
   for(unsigned int i = 0; i < file->size; i++){
     counter += max_line_check(arraylist_get(file, i), n, i);
   }
@@ -17,7 +18,8 @@ unsigned int max_line_numbers(arraylist_t *file, unsigned int n){
 //returns 1 if excess character(s)
 unsigned int max_line_check(char *line, unsigned int n, unsigned int line_index){
   unsigned int counter = 0;
-  for(int i = 0; (int) strlen(line); i++){
+  unsigned int line_length = strlen(line);
+  for(unsigned int i = 0; i < line_length; i++){
     counter++;
   }
   if(counter > n){
