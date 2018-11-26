f620a357a909067a2721405188c164b015b226bd
Moussier <arthur.bensimon@gmail.com>
2018-11-26 13:33:21 +0100

    added freeing of arraylists used in the tests

--- src/test.c
+++ src/test.c
@@ -293,6 +293,8 @@ static void test_rule_max_line_numbers(){
 	check_max_line_length(file, 5);
 	check_max_line_length(file, 50);
 
+	arraylist_free(file, 1);
+
 }
 
 static void test_rule_max_file_line_numbers(){
@@ -308,6 +310,8 @@ static void test_rule_max_file_line_numbers(){
 
 	max_file_line_numbers(file, 2);
 	max_file_line_numbers(file, 50);
+
+	arraylist_free(file, 1);
 
 }
 
