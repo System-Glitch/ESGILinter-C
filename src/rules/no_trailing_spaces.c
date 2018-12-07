8d9f11d552f3fa010726673afba61ce689a653c6
Moussier <arthur.bensimon@gmail.com>
2018-12-07 10:58:50 +0100

    Use real lines instead of parsed lines. Add rn case support

--- src/rules/no_trailing_spaces.c
+++ src/rules/no_trailing_spaces.c
@@ -7,18 +7,21 @@ int no_trailing_spaces(arraylist_t *file){
   if(!file) return -1;
   unsigned int counter = 0;
   for(int i = 0; i < file-> size; i++){
-    if()
+    counter += no_trailing_spaces_check(arraylist_get(file, i));
   }
   return counter;
 }
 
-int no_trailing_spaces_check(line_t *line){
-  if(!line) return 0;
+int no_trailing_spaces_check(char *line){
   int index;
-  index = strlastindexof(line->line, '\n');
-  if(index >= 1 && line->line[index-1] == ' '){
-        print_warning(line->source, line->real_line, line->line, "Trailing space(s) before EOL");
-        return 1;
+  index = strlastindexof(line, "\n");
+  if(index >= 1 && line[index-1] == " "){
+    print_warning(line->source, line->real_line, line->line, "Trailing space(s) before EOL");
+    return 1;
+  }
+  else if(index >= 1 && line[index - 1] == "\r" && line[index - 2] == " "){
+    print_warning(line->source, line->real_line, line->line, "Trailing space(s) before EOL");
+    return 1;
   }
   return 0;
 }
