ea29f56c16ab73fe1945febf66e231d7518331aa
SystemGlitch <jeremy.la@outlook.fr>
2018-12-09 14:39:23 +0100

    Fix no-trailing-space rule compilation

--- src/rules/no_trailing_spaces.c
+++ src/rules/no_trailing_spaces.c
@@ -1,27 +1,23 @@
 #include "rules/no_trailing_spaces.h"
-#include <string.h>
 #include "stringutils.h"
-#include "fileloader.h"
+#include "display.h"
 
-int no_trailing_spaces(arraylist_t *file){
-  if(!file) return -1;
-  unsigned int counter = 0;
-  for(int i = 0; i < file-> size; i++){
-    counter += no_trailing_spaces_check(arraylist_get(file, i));
-  }
-  return counter;
+static int no_trailing_spaces_check(char *line, int line_index, char *filename) {
+	int index;
+	index = strlastindexof(line, '\n');
+	if((index >= 1 && line[index-1] == ' ') || (index >= 2 && line[index - 1] == '\r' && line[index - 2] == ' ')){
+		print_warning(filename, line_index + 1, line, "Trailing space(s) before EOL"); //TODO trim
+		return 1;
+	}
+	return 0;
 }
 
-int no_trailing_spaces_check(char *line){
-  int index;
-  index = strlastindexof(line, "\n");
-  if(index >= 1 && line[index-1] == " "){
-    print_warning(line->source, line->real_line, line->line, "Trailing space(s) before EOL");
-    return 1;
-  }
-  else if(index >= 1 && line[index - 1] == "\r" && line[index - 2] == " "){
-    print_warning(line->source, line->real_line, line->line, "Trailing space(s) before EOL");
-    return 1;
-  }
-  return 0;
+int no_trailing_spaces(arraylist_t *file, char *filename) {
+	if(!file) return -1;
+	unsigned int counter = 0;
+	for(size_t i = 0 ; i < file->size ; i++){
+		counter += no_trailing_spaces_check(arraylist_get(file, i), i, filename);
+	}
+	return counter;
 }
+
