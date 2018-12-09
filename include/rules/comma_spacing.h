0cc294b787056a60cfd2df87ecae9c0a56ebd9e0
Pierre Delmer <d.pierrebox@gmail.com>
2018-12-09 15:08:46 +0100

    Adding comma_spacing rule

---
+++ include/rules/comma_spacing.h
@@ -0,0 +1,12 @@
+#ifndef ESGILINTER_C_COMMA_SPACING_H
+#define ESGILINTER_C_COMMA_SPACING_H
+
+#include <string.h>
+#include "arraylist.h"
+#include "stringutils.h"
+#include "display.h"
+#include "fileloader.h"
+
+unsigned int comma_spacing(arraylist_t *buffer);
+
+#endif //ESGILINTER_C_COMMA_SPACING_H
