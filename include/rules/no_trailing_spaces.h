603699f919d2cd94d22a1db21b043d3ff3be7b41
SystemGlitch <jeremy.la@outlook.fr>
2018-12-09 14:39:23 +0100

    Fix no-trailing-space rule compilation

--- include/rules/no_trailing_spaces.h
+++ include/rules/no_trailing_spaces.h
@@ -2,7 +2,7 @@
 #define NO_TRAILING_SPACES_H
 
 #include "arraylist.h"
-#include <string.h>
 
-int no_trailing_spaces(arraylist_t *file);
-int no_trailing_spaces_check(char *line)
+int no_trailing_spaces(arraylist_t *file, char *filename);
+
+#endif
