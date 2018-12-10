f9708d545e5f6134021198430f6fbbf043dc2c9d
SystemGlitch <jeremy.la@outlook.fr>
2018-12-10 22:27:14 +0100

    Update array-bracket-eol display

--- include/rules/array_bracket_eol.h
+++ include/rules/array_bracket_eol.h
@@ -1,16 +1,10 @@
 #ifndef ARRAY_BRACKET_EOL_H
 #define ARRAY_BRACKET_EOL_H
 
-#include <string.h>
 #include "arraylist.h"
 #include "stringutils.h"
 #include "display.h"
 #include "fileloader.h"
 
-unsigned int array_bracket_eol(arraylist_t *file);
-
-unsigned int array_bracket_eol_check(line_t *line);
-
-unsigned int check_do(char *line, int index_bracket);
-
+unsigned int array_bracket_eol(arraylist_t *file, arraylist_t *real_file);
 #endif
--- src/rules/array_bracket_eol.c
+++ src/rules/array_bracket_eol.c
@@ -1,57 +1,59 @@
 #include "rules/array_bracket_eol.h"
 
-unsigned int array_bracket_eol(arraylist_t *file){
-  unsigned int counter = 0;
+static unsigned int check_do(char *line, int index_bracket){
+	if(index_bracket == -1){
+		return -1;
+	}
+	for(int i = index_bracket - 1; i > 0; i--){
+		if(line[i] == 'o' && line[i-1] == 'd'){
+			return i;
+		}else if(!is_whitespace(line[i])){
+			return -1;
+		}
+	}
+	return -1;
+}
 
-	for(unsigned int i = 0; i < file->size; i++){
-		counter += array_bracket_eol_check(get_line(file, i));
+static unsigned int array_bracket_eol_check(line_t *line, arraylist_t *real_file) {
+	char *tmp = str_remove_comments(line->line);
+	int index_line_break = strlastindexof(tmp, '\n');
+	int index_parenthesis = strlastindexof(tmp, ')'); // can be null in do{ case
+	int index_bracket = strlastindexof(tmp, '{');
+	int index_do;
+	char *display = NULL;
+	unsigned int breaks = 0;
+
+	if(index_bracket != -1 && index_line_break != -1 && !check_quotes(tmp, tmp + index_bracket, strlen(tmp))){
+		index_do = check_do(tmp, index_bracket);
+		if(index_do != -1){
+			for(int i = index_do + 1; i < index_bracket; i++){
+				if(tmp[i] == '\n'){
+					breaks = strcount_heading_line_breaks(line->line) + (line->start_real_line == 0 ? 1 : 0);
+					display = trim(arraylist_get(real_file, line->start_real_line + breaks - 1));
+					print_warning(line->source, line->start_real_line + breaks, display, "Bracket is not end of line");
+					free(tmp);
+					free(display);
+					return 1;
+				}
+			}
+		} else if(index_parenthesis != -1 && index_line_break > index_parenthesis && index_line_break < index_bracket){
+			breaks = strcount_heading_line_breaks(line->line) + (line->start_real_line == 0 ? 1 : 0);
+			display = trim(arraylist_get(real_file, line->start_real_line + breaks - 1));
+			print_warning(line->source, line->start_real_line + breaks, display, "Bracket is not end of line");
+			free(tmp);
+			free(display);
+			return 1;
+		}
+	}
+	free(tmp);
+	return 0;
+}
+
+unsigned int array_bracket_eol(arraylist_t *file, arraylist_t *real_file) {
+	unsigned int counter = 0;
+
+	for(unsigned int i = 0; i < file->size; i++) {
+		counter += array_bracket_eol_check(get_line(file, i), real_file);
 	}
 	return counter;
-}
-
-unsigned int array_bracket_eol_check(line_t *line){
-  char *tmp = str_remove_comments(line->line);
-  int index_line_break = strlastindexof(tmp, '\n');
-  int index_parenthesis = strlastindexof(tmp, ')'); // can be null in do{ case
-  int index_bracket = strlastindexof(tmp, '{');
-  int index_do;
-
-  if(index_bracket != -1 && index_line_break != -1 && !check_quotes(tmp, tmp + index_bracket, strlen(tmp))){
-    index_do = check_do(tmp, index_bracket);
-    if(index_do != -1){
-      for(int i = index_do + 1; i < index_bracket; i++){
-        if(tmp[i] == '\n'){
-          print_warning(line->source, line->start_real_line, line->line, "Bracket is not end of line");
-          free(tmp);
-          return 1;
-        }
-      }
-    }else if(index_parenthesis != -1 && index_line_break > index_parenthesis && index_line_break < index_bracket){
-      print_warning(line->source, line->start_real_line, line->line, "Bracket is not end of line");
-      free(tmp);
-      return 1;
-    }else{
-      free(tmp);
-      return 0;
-    }
-  }else{
-    free(tmp);
-    return 0;
-  }
-  free(tmp);
-  return 0;
-}
-
-unsigned int check_do(char *line, int index_bracket){
-  if(index_bracket == -1){
-    return -1;
-  }
-  for(int i = index_bracket - 1; i > 0; i--){
-    if(line[i] == 'o' && line[i-1] == 'd'){
-      return i;
-    }else if(!is_whitespace(line[i])){
-      return -1;
-    }
-  }
-  return -1;
 }
